4de299d1436a282200c353b4cf81311cfd70903d
ani5ruth <45996427+ani5ruth@users.noreply.github.com>
2020-04-09 17:16:53 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,4 +1,3 @@
-# importing libraries
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize, sent_tokenize
@@ -16,7 +15,6 @@ class Summarizer:
 
     def construct_stems(self):
         '''Construct word stems using porter stemming algorithm'''
-        
         stop_words = set(stopwords.words("english"))
         stemmer = PorterStemmer()
         self.stems = list()
@@ -35,27 +33,21 @@ class Summarizer:
 
     def compute_sentence_score_table(self) -> dict:
         '''Returns a score table of every sentence in article'''
-        
         # for each sentence cumulate the weights of its words
         word_counter = self.compute_word_weight_table()
         score_table = dict()
         for sentence in self.sentences:
             stems_in_sent = list(filter(lambda stem: stem in sentence.lower(), word_counter))
-            
-            # for every word in sentence that is in frequncy table sum the weights
-            for stem in stems_in_sent:
-                score_table[sentence] = score_table.get(sentence, 0) + word_counter[stem]
-                
+            # accumulate every frequency of stems in sentence
+            total_sentence_score = sum(list(map(lambda stem: word_counter[stem], stems_in_sent)))
             # average out weight to avoid long senteces to get higher preference
-            score_table[sentence] = score_table.get(sentence, 0) / len(stems_in_sent)
-            
+            score_table[sentence] = total_sentence_score / len(stems_in_sent)
+
         return score_table
-    
+
     def get_article_summary(self):
         '''Returns summary of article using threshold-limited sentence ranking algorithm'''
-           
         score_table = self.compute_sentence_score_table()
         threshold = 1.5 * sum(score_table.values())/len(score_table)
-    
         return "".join(list(filter(lambda sentence: score_table.get(sentence, 0) >= threshold, score_table)))
 

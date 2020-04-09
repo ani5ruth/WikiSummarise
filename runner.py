e0540487df60b0884d557281ce034c98bcf292bb
ani5ruth <45996427+ani5ruth@users.noreply.github.com>
2020-04-09 18:09:11 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -5,14 +5,13 @@ from summarizer import Summarizer
 if __name__ == '__main__':
     while True:
         keyword = input("\n\nEnter keyword of wikipedia article to summarize (eg. Covid-19) or type exit to quit: ")
-        
         if keyword == "exit":
             exit(1)
-            
+
         article = get_article(keyword)
         if article == None:
             print("\n\nCould not find article matching keyword - " + keyword)
             continue
-
+        
         print("\n\nOriginal Artcile : "  + article['original'])
-        print("\n\nSummarised Article : \n" + Summarizer(article['text']).get_article_summary())
+        print("\n\nSummarised Article : \n" + Summarizer(article['text']).get_article_summary(1.5))
